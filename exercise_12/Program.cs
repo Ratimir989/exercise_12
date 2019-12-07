647c1c82bfa66db9d4b4ac85ba35b88e6c5e38ed
Ratimir <ra0953029364@gmail.com>
2019-12-07 21:36:41 +0200

    Добавьте файлы проекта.

---
+++ exercise_12/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace exercise_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("введите число N");
+            int N = int.Parse(Console.ReadLine());
+            for (int i = 1; i < N; i++)
+            {
+                Console.WriteLine($"{i}^{i}={i * i}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
